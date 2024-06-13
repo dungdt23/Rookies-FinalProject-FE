b599ea426dd629919d0f0cefdf04372f8832225a
JaclyNolan <anhbg330011@gmail.com>
2024-06-13 15:17:44 +0700

    Added API Constants

--- src/constants/axiosConstants.ts
+++ src/constants/axiosConstants.ts
@@ -1,5 +1,5 @@
 export const AxiosConstants = {
-    AXIOS_BASEURL: "http://localhost:5000",
+    AXIOS_BASEURL: "https://rookies-b7-g3-api.azurewebsites.net",
     AXIOS_TIMEOUT: 10000,
     AXIOS_HEADER: {'Content-Type': 'application/json'},
 }
