14dfb44fcd0335b0834a99351d3c1ddc6a0b8c10
JaclyNolan <anhbg330011@gmail.com>
2024-06-14 07:45:36 +0700

    Update axios Constant & GH workflow to get the right base url for prod and dev env

--- src/constants/axiosConstants.ts
+++ src/constants/axiosConstants.ts
@@ -1,5 +1,5 @@
 export const AxiosConstants = {
-    AXIOS_BASEURL: "https://rookies-b7-g3-api.azurewebsites.net",
+    AXIOS_BASEURL: process.env.API_URL || "http://localhost:5138",
     AXIOS_TIMEOUT: 10000,
     AXIOS_HEADER: {'Content-Type': 'application/json'},
 }
