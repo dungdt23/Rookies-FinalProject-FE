5926bb3f4e9ae3d1ffb45619b28b9808c3b7f47f
NIwnla <sonnguyenvietbao@gmail.com>
2024-06-20 11:03:17 +0700

    Update assignment.service.ts

---
+++ src/services/assignment.service.ts
@@ -0,0 +1,11 @@
+import { UserType } from "../types/user";
+
+export interface GetAllAssignmentParams{
+    locationIdL:string;
+    userId?:string;
+    userType:UserType;
+    searchString?:string;
+    isAscending:boolean;
+    fieldFilter:
+}
+
